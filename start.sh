35a5835525e35d58effc90486be46650327d8849
Pol <145445253+PolCerrilloITB@users.noreply.github.com>
2025-03-03 20:02:48 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
-service ssh start
-chown -R developer:developer /home/developer
-su - developer -c "vncserver :1 -geometry 1920x1080 -depth 24 -SecurityTypes VncAuth -xstartup /home/developer/.vnc/xstartup"
-tail -f /dev/null
+
+# Cambiar al usuario developer
+if [ "$(whoami)" != "developer" ]; then
+    echo "Este script debe ejecutarse como el usuario 'developer'"
+    exec sudo -u developer "$0" "$@"
+    exit 1
+fi
+
+# Crear el directorio y establecer la contraseña de VNC
+mkdir -p /home/developer/.vnc
+echo "itb2023" | vncpasswd -f > /home/developer/.vnc/passwd
+chmod 600 /home/developer/.vnc/passwd
+chown -R developer:developer /home/developer/.vnc
+
+# Crear archivo .Xauthority
+touch /home/developer/.Xauthority
+chown developer:developer /home/developer/.Xauthority
+
+# Iniciar Xvfb para emular un servidor X
+Xvfb :0 -screen 0 1024x768x24 &
+
+# Establecer la variable DISPLAY
+export DISPLAY=:0
+
+# Iniciar VNC
+x11vnc -forever -display :0 -passwd itb2023 &
+
+# Iniciar servicio SSH
+sudo service ssh start
+
+# Inicia GNOME Terminal
+gnome-terminal --display=:0 &
+
+# Iniciar el entorno XFCE
+exec xfce4-session
